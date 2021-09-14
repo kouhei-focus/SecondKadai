baaedf0df28b50e8fb522075a68a003f4dea2ea2
kouhei-focus <mpz7kouhei@gmail.com>
2021-09-14 14:15:07 +0900

    label,button,textFieldを配置した

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textField.delegate = self
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    
+   
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let resultVC = segue.destination as! ResultViewController
+        
+        print(textField.text!)
+        resultVC.labelText = textField.text!
+    }
+    
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+        
+    }
 }
 
